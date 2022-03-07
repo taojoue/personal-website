5247811bbf75b21ad4be3580d386ba2eb66b4743
taojoue <taojouet@gmail.com>
2022-03-07 23:02:14 +0100

    Finished landing page

---
+++ components/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+// import Navbar from './navbar'
+
+/* omment */
+const Layout = ({ children }) => {
+    return (
+        <div>
+            {/* <Navbar />
+            <main>
+                {children}
+            </main>
+            <Footer/> */}
+        </div>
+    )
+}
+
+export default Layout
---
+++ tailwind.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+        '1': '8px',
+        '2': '12px',
+        '3': '16px',
+        '4': '24px',
+        '5': '32px',
+        '6': '48px',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    },
+    container: {
+      center: true
+    }
+  },
+  plugins: [],
+}
